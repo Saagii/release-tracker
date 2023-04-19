7dcc7fbeb976cbd41e2cc105bd9f9e6bbfef0e65
Sagar Desai <sagardesai627@gmail.com>
2023-04-20 00:51:01 +0530

    Admin Component UI updated.

--- src/app/modules/main/content/components/admin/admin.component.ts
+++ src/app/modules/main/content/components/admin/admin.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { StatusService } from 'src/app/modules/shared/services/status.service';
+import { adminMenu } from '../../data/admin-menu-mock-data';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html'
 })
 export class AdminComponent implements OnInit {
+
+  adminMenu: any[] = adminMenu;
 
   constructor(
     private statusService: StatusService
---
+++ src/app/modules/main/content/data/admin-menu-mock-data.ts
@@ -0,0 +1,56 @@
+export const adminMenu = [
+    {
+        icon: 'people',
+        title: 'Members & Invites',
+        subtitle: 'View Members & Invite new member to your team.',
+        route: 'members'
+    },
+    {
+        icon: 'assessment',
+        title: 'Reports & Analytics',
+        subtitle: 'Generate organised & structured reports to gain complete insights on the progress.',
+        route: 'reports'
+    },
+    {
+        icon: 'recent_actors',
+        title: 'Roles & Permissions',
+        subtitle: "Manage member's authority based on the permissions assigned to them.",
+        route: 'roles'
+    },
+    {
+        icon: 'featured_play_list',
+        title: 'System Configurations',
+        subtitle: 'Set up your own system configurations that alligns with your progress.',
+        route: 'clients'
+    },
+    {
+        icon: 'play_for_work',
+        title: 'Integration Settings',
+        subtitle: 'Configure required integration that helps in tracking your projects.',
+        route: 'timelines'
+    },
+    {
+        icon: 'security',
+        title: 'Security Settings',
+        subtitle: 'Define your custom password policies and other access control settings. ',
+        route: 'admin'
+    },
+    {
+        icon: 'insert_drive_file',
+        title: 'Documentations',
+        subtitle: 'View & Download the required documents.',
+        route: 'timelines'
+    },
+    {
+        icon: 'forum',
+        title: 'FAQs',
+        subtitle: 'Search, View or Add FAQs',
+        route: 'admin'
+    },
+    {
+        icon: 'contact_phone',
+        title: 'Support',
+        subtitle: 'Contact Support desk for any queries.',
+        route: 'admin'
+    }
+];
