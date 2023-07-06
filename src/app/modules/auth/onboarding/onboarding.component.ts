087be29126532235e0888f504e6e0d1eed03e20d
Sagar Desai <sagardesai627@gmail.com>
2023-07-07 02:09:23 +0530

    Onboarding Component: Tabs Created, Organsation Info content added. To Proceed with further Admin Info and Confirmation

--- src/app/modules/auth/onboarding/onboarding.component.ts
+++ src/app/modules/auth/onboarding/onboarding.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-onboarding',
@@ -6,7 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class OnboardingComponent implements OnInit {
 
-  constructor() {}
+  orgInfoForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.orgInfoForm = this.fb.group({
+      orgName: [null, Validators.required],
+      orgType: [null, Validators.required]
+    });
+  }
 
   ngOnInit(): void {}
   
