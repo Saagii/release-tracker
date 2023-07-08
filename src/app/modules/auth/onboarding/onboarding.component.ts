705ac81eb79ae86b7d8234b7fb9eefd7708217e8
Sagar Desai <sagardesai627@gmail.com>
2023-07-08 19:25:13 +0530

    Onboarding Page Updated with Validations. Ready to have it integrated with APIs.

--- src/app/modules/auth/onboarding/onboarding.component.ts
+++ src/app/modules/auth/onboarding/onboarding.component.ts
@@ -8,16 +8,64 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class OnboardingComponent implements OnInit {
 
   orgInfoForm: FormGroup;
+  adminInfoForm: FormGroup;
+  verificationForm: FormGroup;
+  hidePassword = true;
+  onBoardingState: number = 1;
 
   constructor(
     private fb: FormBuilder
   ) {
+
+    // Organization info
     this.orgInfoForm = this.fb.group({
       orgName: [null, Validators.required],
       orgType: [null, Validators.required]
     });
+
+    // Admin info
+    this.adminInfoForm = this.fb.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [null, Validators.required],
+      password: [null, Validators.required]
+    });
+
+    // Verification Code
+    this.verificationForm = this.fb.group({
+      verificationCode: [null, Validators.required]
+    });
   }
 
   ngOnInit(): void {}
+
+  /*
+    Tab Navigation Methods
+  */
+  tabNavigation(tabNumber: number): void {
+    this.onBoardingState = this.onBoardingState + tabNumber;
+  }
+
+
+  /*
+    Validate form to enable tab navigation.
+  */
+  validateForm(): boolean {
+    if(this.onBoardingState === 1) {
+      return this.orgInfoForm.valid;
+    } else if(this.onBoardingState === 2) {
+      return this.adminInfoForm.valid;
+    }
+    
+    return false;
+  }
+
+
+  /*
+    Verfiy and Create Account Method
+  */
+  verifyAndCreateAccount(): void {
+    console.log('Account Created...');
+  }
   
 }
