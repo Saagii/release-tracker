d8c2a66ddc633ad405af2494fd1daf7c86f0626d
Sagar Desai <sagardesai627@gmail.com>
2023-08-21 00:14:26 +0530

    Client View Page updated with clients list API

--- src/app/modules/main/content/components/clients/clients.component.ts
+++ src/app/modules/main/content/components/clients/clients.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { StatusService } from 'src/app/modules/shared/services/status.service';
 import { clientsMockData } from '../../data/clients-mock-data';
 import { ClientsService } from '../../../services/clients.service';
+import { ClientConfigurationResponse } from '../../model/clientConfigurationResponse.model';
 
 @Component({
   selector: 'app-clients',
@@ -9,7 +10,8 @@ import { ClientsService } from '../../../services/clients.service';
 })
 export class ClientsComponent implements OnInit {
 
-    clientsList: any[] = clientsMockData;
+    clientsList: any;
+    clientConfig: ClientConfigurationResponse | undefined;
     clientsDisplayColumns: string[] = ['clientName', 'lables', 'totalProjects', 'totalReleases', 'actions'];
 
   constructor(
@@ -18,7 +20,9 @@ export class ClientsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getClientsList();
+
+    // Get Clients Configurations.
+    this.getClientConfigurations();
   }
 
   /*
@@ -33,9 +37,39 @@ export class ClientsComponent implements OnInit {
     Get Clients List.
   */
   getClientsList(): void {
-    this.clientsService.getClientsList().subscribe((response: any) => {
+    this.clientsService.getClientsList().subscribe((response: any[]) => {
       console.log(response);
+
+      this.clientsList = response;
     });
+  }
+
+
+  /*
+    Get Client Configurations.
+  */
+  getClientConfigurations(): void {
+    this.clientsService.getClientsConfig().subscribe((response: ClientConfigurationResponse) => {
+      console.log(response);
+
+      this.clientConfig = response;
+
+      // Get Clients List
+      this.getClientsList();
+    });
+  }
+
+
+  /*
+    Filter Client type based on ID and return the type value.
+  */
+  filterClientType(clientTypeId: string): any {
+    const clientTypesConfig: any = this.clientConfig?.types;
+    for(const client of clientTypesConfig) {
+      if(client._id === clientTypeId) {
+        return client.value;
+      }
+    }
   }
   
 }
