1e336614c71d7374d5e7e0798ef562afe83f63be
Sagar Desai <sagardesai627@gmail.com>
2023-10-13 01:08:07 +0530

    Release Creation Component integrated with GET APIs. To continue with POST api.

--- src/app/modules/main/content/components/releases/components/release-create-edit/release-create-edit.component.ts
+++ src/app/modules/main/content/components/releases/components/release-create-edit/release-create-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClientsService } from 'src/app/modules/main/services/clients.service';
+import { ReleasesService } from 'src/app/modules/main/services/releases.service';
 import { StatusService } from 'src/app/modules/shared/services/status.service';
 
 @Component({
@@ -9,11 +11,15 @@ import { StatusService } from 'src/app/modules/shared/services/status.service';
 export class ReleaseCreateEditComponent implements OnInit {
 
     releaseCreateEditForm: FormGroup;
+    clientsList: any;
+    releaseConfigDetails: any;
 
 
   constructor(
     private statusService: StatusService,
     private fb: FormBuilder,
+    private clientService: ClientsService,
+    private releaseService: ReleasesService
   ) {
      // Prepare Sign In Form
      this.releaseCreateEditForm = this.fb.group({
@@ -28,13 +34,52 @@ export class ReleaseCreateEditComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    // Get Clients List
+    this.getClientsList();
+
+    // Get release config details.
+    this.getReleaseConfigDetails();
+  }
 
   /*
     Get Status Style
   */
   getStatusStyle(statusValue: string): any {
     return this.statusService.getStatusStyle(statusValue);
+  }
+
+
+  /*
+    Get Clients List.
+  */
+  getClientsList(): any {
+    this.clientService.getClientsList().subscribe((clients: any[]) => {
+      console.log(clients);
+
+      this.clientsList = clients;
+    });
+  }
+
+
+  /*
+    Get Release config details.
+  */
+  getReleaseConfigDetails(): any {
+    this.releaseService.getReleasesConfig().subscribe((releaseConfig: any) => {
+      console.log(releaseConfig);
+
+      this.releaseConfigDetails = releaseConfig;
+    })
+  }
+
+
+  /*
+    Submit release details.
+  */
+  submiteReleaseDetails(): any {
+    
   }
   
 }
