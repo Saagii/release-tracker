c507e4a90b061d1f2c5e9775b0e9015f10653c83
Sagar Desai <sagardesai627@gmail.com>
2024-04-16 00:28:35 +0530

    UI updates on clients view page.

--- src/app/modules/main/content/components/clients/components/client-profile/client-profile.component.ts
+++ src/app/modules/main/content/components/clients/components/client-profile/client-profile.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { StatusService } from 'src/app/modules/shared/services/status.service';
 import { clientProfileMockData } from '../../../../data/client-profile-mock-data';
 import { requirementsListMockData } from '../../../../data/requirements-list-mock-data';
+import { ReleasesService } from 'src/app/modules/main/services/releases.service';
+import { ActivatedRoute } from '@angular/router';
+import { ClientsService } from 'src/app/modules/main/services/clients.service';
+import { MembersService } from 'src/app/modules/main/services/members.service';
+import { ProjectsService } from 'src/app/modules/main/services/projects.service';
 
 @Component({
   selector: 'app-client-profile',
@@ -13,12 +18,33 @@ export class ClientProfileComponent implements OnInit {
   requirementsPanelOpenState = false;
   viewMoreReqDetails: string = '';
   phases = requirementsListMockData;
+  projectId: string = '';
+  projectDetails: any;
+  projectConfig: any;
+  clientsList: any;
+  tempStatusValue: string = '';
+  teamMembersCount: number = 0;
+  teamMembersDetailsList: any = [];
+  releasesList: any = [];
+  releaseConfigDetails: any;
+  selectedReleaseDetails: any;
 
   constructor(
-    private statusService: StatusService
+    private statusService: StatusService,
+    private projectsService: ProjectsService,
+    private _activatedRoute: ActivatedRoute,
+    private clientsService: ClientsService,
+    private membersService: MembersService,
+    private releasesService: ReleasesService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Get project configurations.
+    this.getProjectConfigurations();
+
+    // Get Release Config details
+    this.getReleaseConfigDetails();
+  }
 
   /*
     Get Status Style
@@ -32,6 +58,101 @@ export class ClientProfileComponent implements OnInit {
   */
   toggleRequirementView(toggleValue: any): void {
     this.viewMoreReqDetails = toggleValue;
+  }
+
+
+    /* 
+    Get Project Configurations.
+  */
+  getProjectConfigurations(): void {
+    this.projectsService.getProjectsConfig().subscribe((projectConfig: any) => {
+      console.log(projectConfig);
+
+      this.projectConfig = projectConfig;
+    })
+  }
+
+
+      /*
+    Get release config details.
+  */
+  getReleaseConfigDetails(): any {
+    this.releasesService.getReleasesConfig().subscribe((releaseConfig: any) => {
+      console.log(releaseConfig);
+
+      this.releaseConfigDetails = releaseConfig;
+    });
+  }
+
+
+  /*
+    Get Clients List.
+  */
+  getClientsList(): void {
+    this.clientsService.getClientsList().subscribe((response: any[]) => {
+      console.log(response);
+      
+      this.clientsList = response;
+    });
+  }
+
+
+      /*
+    Filter clients, release config IDs.
+  */
+  filterRequiredIds(type: string, id: string): any {
+    
+    // Return client name.
+    if(type === 'client') {
+      return this.clientsList.filter((client: any) => {
+         return client._id === id;
+      })[0].clientName;
+    }
+
+    // Return project status.
+    if(type === 'status') {
+      return this.projectConfig.status.filter((status: any) => {
+        return status._id === id;
+      })[0].value;
+    }
+
+    // Return project status.
+    if(type === 'release_status') {
+      return this.releaseConfigDetails.status.filter((status: any) => {
+        return status._id === id;
+      })[0].value;
+    }
+
+    // Return Target value.
+    if(type === 'target') {
+      return this.releaseConfigDetails.targets.filter((target: any) => {
+         return target._id === id;
+      })[0]?.value;
+    }
+
+    // Return Type value.
+    if(type === 'type') {
+      return this.releaseConfigDetails.types.filter((type: any) => {
+         return type._id === id;
+      })[0]?.value;
+    }
+
+    // Return Type value.
+    if(type === 'priorities') {
+      return this.releaseConfigDetails.priorities.filter((priority: any) => {
+         return priority._id === id;
+      })[0]?.value;
+    }
+
+    // Return member name.
+    if(type === 'teamMember') {
+      console.log(this.teamMembersDetailsList);
+      const memberDetails = this.teamMembersDetailsList.filter((member: any) => {
+        return member.id === id;
+      })[0];
+
+      return memberDetails.firstName + ' ' + memberDetails.lastName;
+    }
   }
   
 }
